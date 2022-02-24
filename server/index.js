7cb5cd52a596e565086fdd6cb0fc2a092f2a8f2a
Andrew Corry <alcorry112@outlook.com>
2022-02-24 13:36:32 -0600

    Initial Commit3

--- server/index.js
+++ server/index.js
@@ -20,6 +20,19 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
+let students = []
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('student was added successfully', {author: 'Andrew', type:'Manual entry',})
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
+
 const port = process.env.PORT || 4545;
 
 app.listen(port, ()=>{
