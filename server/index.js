ea319536918f425f0e74fd25d9dee6f23f5adfab
Andrew Corry <alcorry112@outlook.com>
2022-02-24 13:11:25 -0600

    Initial Commit3

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '9864a83d3ede4a6ba47975bfaa23ca5e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const port = process.env.PORT || 4545;
 
 app.listen(port, ()=>{
